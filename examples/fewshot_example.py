1d77dbb61d69cf978940f307b1c908387b243eb8
SanchoPanso <20alexchernik@gmail.com>
2024-05-21 11:58:54 +0200

    feat: update example

--- examples/fewshot_example.py
+++ examples/fewshot_example.py
@@ -1,3 +1,5 @@
+import os
+import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 from fewshot_samclip.fewshot_detector import FewShotDetector
@@ -7,19 +9,21 @@ from fewshot_samclip.utils import show_anns
 def main():
     detector = FewShotDetector()
     query_images = [
-        Image.open("images/query_image.jpg"), 
-        # Image.open("path_to_query_image2.jpg")
+        Image.open("images/query_image_1.jpg"), 
+        Image.open("images/query_image_2.jpg"),
     ]
     detector.set_queries(query_images)
 
     image = Image.open("images/target_image.jpg")
-    selected_masks = detector(image, 0.95)
+    selected_masks = detector(image, 0.85)
     
     plt.figure(figsize=(20,20))
     plt.imshow(image)
     show_anns(selected_masks)
     plt.axis('off')
-    plt.savefig('plt_show.jpg') 
+
+    os.makedirs('outs', exist_ok=True)
+    plt.savefig('outs/plt_show.jpg') 
 
 
 if __name__ == '__main__':
